0b23d2f7bb659c629ee1b2fe8693429b3d7e807e
bsperry <69691639+bsperry@users.noreply.github.com>
2021-02-03 19:43:52 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assignment3.Models;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //for the home page view (contains the picture of joel)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //for the podcast page view (contains link to baconsale
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        //for loading the page
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        //for the movies form 
+        [HttpPost]
+        public IActionResult Movies(NewMovie movie)
+        {
+            TempStorage.AddMovie(movie);
+            return View("Confirmation", movie);
+        }
+
+        //for listing/viewing all the movies
+
+        public IActionResult ListMovies()
+        {
+            return View(TempStorage.Movies);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/NewMovie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class NewMovie
+    {
+        [Required(ErrorMessage = "Please select the movie category.")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "Please enter the movie title.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter the movie year(s).")]
+        public string Year { get; set; }
+        [Required(ErrorMessage = "Please enter the movie director.")]
+        public string Director { get; set; }
+        [Required(ErrorMessage ="Please select the movie rating.")]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Lent { get; set; }
+        [StringLength(25,ErrorMessage ="Notes can only be 25 characters long.") ]
+        public string Notes { get; set; }
+
+    }
+}
